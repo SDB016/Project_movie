9790edf343e049f86f539a969a938071e8ce0af0
shin <dongbin4013@naver.com>
2020-02-07 04:52:07 +0900

    텍스트 파일 읽고 열려있는 텍스트파일 이름 가져오기

--- project_movie/ExamWin32.cpp
+++ project_movie/ExamWin32.cpp
@@ -1,5 +1,5 @@
-﻿
-#include "pch.h"
+﻿#include "pch.h"
+#include <stdio.h>
 #include "tipsware.h"
 
 struct AppData  // 프로그램에서 사용할 내부 데이터
@@ -20,19 +20,51 @@ void OnCommand(INT32 a_ctrl_id, INT32 a_notify_code, void* ap_ctrl)
     }
     else if (a_ctrl_id == 1001) {
         if (p_data->p_target != NULL) {
-            char str[128];  // 에디트 컨트롤에 입력된 문자열을 저장할 배열 변수
+            char str[64];  // 에디트 컨트롤에 입력된 문자열을 저장할 배열 변수
             int x, y;
             // 메모장의 클라이언트 영역에서 (10, 10) 지점을 화면 좌표로 변환합니다.
             GetMousePosFromTarget(p_data->p_target, &x, &y, 10, 10);
             // 메모장에 글을 쓰기 위해서 (x, y)에 마우스를 클릭해서 메모장을 선택합니다.
             MouseClickWrite(x, y);
             // 에디트 컨트롤에 저장된 문자열을 str 배열에 복사한다.
-            GetCtrlName(FindControl(1010), str, 128);
+            GetCtrlName(FindControl(1010), str, 64);
             // 한글 모드라고 가정하고 str 배열에 저장된 문자열을 메모장에 씁니다.
             InputNormalString(str, 1);
         }
     }
+    else if (a_ctrl_id == 1002) {
+        void* p_list_box = FindControl(1003);
+        ListBox_ResetContent(p_list_box);
+
+        char str[64];
+        HWND h_cur_wnd = NULL;
+
+        while (1) {
+            h_cur_wnd = ::FindWindowEx(NULL, h_cur_wnd, "notepad", NULL);
+            if (h_cur_wnd != NULL) {
+                ::GetWindowText(h_cur_wnd, str, 64);
+                ListBox_InsertString(p_list_box, -1, str, 1);
+            }else break;
+        }
+    }
 }
+
+// 각 음료수별 칼로리를 출력한다.
+void DrawDrinkItem(int index, char* ap_str, int a_str_len, void* ap_data, int a_is_selected, RECT* ap_rect)
+{
+
+    // 각 항목의 배경을 그린다.
+    SelectBrushObject(RGB(62, 77, 104));
+    Rectangle(ap_rect->left, ap_rect->top, ap_rect->right, ap_rect->bottom);
+
+    SelectFontObject("굴림", 12);  // 글꼴을 '굴림', 12 크기로 설정한다.
+    // 음료수 이름과 칼로리 수치를 출력한다.
+    TextOut(ap_rect->left + 6, ap_rect->top + 3, RGB(255, 255, 255), ap_str);
+
+}
+
+
+
 
 // 컨트롤을 조작할 때 호출할 함수를 설정한다.
 CMD_MESSAGE(OnCommand)
@@ -47,16 +79,27 @@ int main()
     AppData data = { NULL };  // 프로그램이 내부적으로 사용할 메모리를 선언한다.
     SetAppData(&data, sizeof(data));  // 지정한 변수를 내부 데이터로 저장한다.
 
-    void* p = CreateListBox(10, 120, 300, 150, 1004);
-    ListBox_AddString(p, "hi", 0);
-    ListBox_AddString(p, "hi", 0);
-    ListBox_AddString(p, "hi", 0);
-    ListBox_AddString(p, "hi", 0);
-    ListBox_AddString(p, "hi", 0);
-
    // 프로그램에서 사용할 버튼을 생성합니다.
     CreateButton("메모장 찾기", 10, 10, 105, 28, 1000);
     CreateButton("메모장에 글쓰기", 120, 10, 105, 28, 1001);
+    CreateListBox(10, 100, 500, 150, 1003);
+    void *p = CreateListBox(530, 100, 600, 600, 1004,DrawDrinkItem);
+    FILE* p_file = NULL;
+    char drink_name[64];
+    // drink.txt 파일을 텍스트 읽기 모드로 연다.
+    if (0 == fopen_s(&p_file, "테스트.txt", "rt")) {
+        // 파일에서 문자열 1개를 읽는다.
+        while (fscanf_s(p_file, "%s", drink_name, 64) == 1) {
+            // 읽어들인 문자열과 정숫값을 리스트 박스에 추가한다.
+            ListBox_SetItemDataEx(p, -1, drink_name, 0);
+        }
+        fclose(p_file);  // 파일을 닫는다!
+    }
+
+    SelectFontObject("굴림", 12); // 글꼴을 '굴림', 12 크기로 변경한다.
+    
+
+    CreateButton("메모장 윈도우 찾기", 10, 270, 160, 28, 1002);
     // 메모장에 쓸 문자열을 입력할 에디트 컨트롤을 생성한다.
     CreateEdit(10, 50, 500, 42, 1000, 0);
     ShowDisplay(); // 정보를 윈도우에 출력한다.
